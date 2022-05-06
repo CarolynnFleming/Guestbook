cfe825dea7ba8fd215588a6506a1155ce174bb2b
CarolynnFleming <cfcatpink@gmail.com>
2022-05-06 11:58:03 -0700

    setting the user in context adn redirecting the to the page that they are trying to get to

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -4,8 +4,14 @@ import { getUser, signInUser } from '../services/user';
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
- const currentUser = getUser();
- const [user, setUser] = userState(currentUser || { email: null });
+  const currentUser = getUser();
+  const [user, setUser] = userState(currentUser || { email: null });
 
- 
-}
+  const login = async (email, password) => {
+    const authenticatedUser = await signInUser({ email, password });
+
+    if (authenticatedUser) {
+      setUser(authenticatedUser);
+    }
+  };
+};
