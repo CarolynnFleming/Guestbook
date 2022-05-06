b823afeba9ed59e37b1512bf659abe269c44e671
CarolynnFleming <cfcatpink@gmail.com>
2022-05-06 10:20:24 -0700

    update entry by id function

--- src/services/entries.js
+++ src/services/entries.js
@@ -14,3 +14,11 @@ export async function createEntry({ userId, content }) {
     .insert({ guest_id: userId, content });
   return parseData(request);
 }
+
+export async function updateEntryById(id, content) {
+    const request = await client
+    .from('entries')
+    .update({ content })
+    .match({ id });
+    return parseData(request);
+}
