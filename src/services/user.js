b73835c8f41dc64e4d3b06003f0884a49afa4efe
CarolynnFleming <cfcatpink@gmail.com>
2022-05-06 10:48:36 -0700

    signup function

--- src/services/user.js
+++ src/services/user.js
@@ -5,5 +5,14 @@ export function getUser() {
 }
 
 export function getSession() {
-    return client.auth.session();
+  return client.auth.session();
 }
+
+export async function signUpUser({ email, password }) {
+  const { user, error } = await client.auth.signUp({ email, password });
+  if (error) throw error;
+
+  return user;
+}
+
+
