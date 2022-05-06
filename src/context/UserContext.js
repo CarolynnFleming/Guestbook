bc068b81db5196b99ba899d1d4a6f3cfd7e9d2b7
CarolynnFleming <cfcatpink@gmail.com>
2022-05-06 11:53:51 -0700

    state and grabbing get user

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,3 +3,9 @@ import { getUser, signInUser } from '../services/user';
 
 export const UserContext = createContext();
 
+export const UserProvider = ({ children }) => {
+ const currentUser = getUser();
+ const [user, setUser] = userState(currentUser || { email: null });
+
+ 
+}
