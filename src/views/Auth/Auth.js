8630584383fafcbd7ec772bfd036163d8fd0b06d
CarolynnFleming <cfcatpink@gmail.com>
2022-05-06 13:12:50 -0700

    set state, locatio, historyand user

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useUser} from '../../context/UserContext';
+
 export default function Auth() {
+    const { login } = useUser();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const location = useLocation();
+    const history = useHistory();
   return (
-    <div>Auth</div>
+   
+
   );
 }
