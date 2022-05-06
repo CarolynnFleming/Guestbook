c7cd7b840bd9449cf21c638053559b0e2f9880e8
CarolynnFleming <cfcatpink@gmail.com>
2022-05-06 11:46:52 -0700

    creating private route

---
+++ src/components/PrivateRoute.js/PrivateRoute.js
@@ -0,0 +1,19 @@
+import { Redirect, Route, useLocation } from 'react-router-dom';
+
+import { user } from '../../context/UserContext';
+
+export default function PrivateRoute({ children, ...rest }) {
+    const { user } = useUser();
+    const location = useLocation();
+
+return (
+    <Route {...rest}>
+        {user.email ? (
+            children ) : (
+                <Redirect to ={{ pathname: "/login", state: {origin: location },
+            }}
+            />
+            )}
+    </Route>
+)
+}
