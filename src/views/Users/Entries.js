b43a1dfe98c7f615b61230eaceed01e86e5450ad
CarolynnFleming <cfcatpink@gmail.com>
2022-05-06 18:49:47 -0700

    displaying logs

--- src/views/Users/Entries.js
+++ src/views/Users/Entries.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import Log from '../../components/ManiaBook/LogForm';
+import Log from '../../components/ManiaBook/Log';
+import LogForm from '../../components/ManiaBook/LogForm';
 import { useUser } from '../../context/UserContext';
 import { getEntries } from '../../services/entires'; 
 
@@ -13,7 +14,38 @@ export default function Entries() {
       .then(setLogs)
       .finally(() => setLoading(false));
   };
+
+  useEffect(() => {
+    fetchLogs();
+  }, []);
   return (
-    <div>Entries</div>
+    <>
+      <LogForm addLog={fetchLogs} />
+      {loading ? (
+        <p>Loading Logs...</p>
+      ) : (
+        <>
+          <h1>Past Logs</h1>
+          <ul>
+            {logs.length ? (
+              logs.map(({ id, description, created_at }) => {
+                return(
+                  <li key={id}>
+                    <Log
+                      description={description}
+                      author={user.email}
+                      date={created_at}/>
+                  </li>
+                );
+              })
+            ) : (
+              <li>
+                        Write Something!
+              </li>
+            )}
+          </ul>
+        </>
+      )}
+    </>
   );
 }
